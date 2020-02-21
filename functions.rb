4aad8ef2b431ca63f2f31e0014789e8081a4ca82
easy <samanoskeakeshi@gmail.com>
2020-02-21 15:51:10 +0500

    add functions

---
+++ functions.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+def accelerate
+  puts 'Stepping on the gas'
+  puts 'Speeding up'
+end
+
+def sound_horn
+  puts 'Pressing the horn button'
+  puts 'Beep beep!'
+end
+
+def use_headlights(brightness)
+  puts "Turning on #{brightness} headlights"
+  puts 'Watch out for deer!'
+end
