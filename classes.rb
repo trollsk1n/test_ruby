bfb90335586132012d37a05666d59fb69a781b77
easy <samanoskeakeshi@gmail.com>
2020-02-24 15:32:17 +0500

    update classes

--- classes.rb
+++ classes.rb
@@ -2,34 +2,44 @@
 
 # Bird class
 class Bird
+  attr_accessor :name
+
   def talk
-    puts 'Chirp! Chirp!'
+    puts "#{@name} says Chirp! Chirp!"
   end
 
   def move(destination)
-    puts "Flying to the #{destination}."
+    puts "#{@name} flies to the #{destination}."
   end
 end
 
 # Another class for dogs
 class Dog
+  attr_accessor :name, :age
+
   def talk
-    puts 'Bark!'
+    puts "#{@name} says Bark!"
   end
 
   def move(destination)
-    puts "Running to the #{destination}."
+    puts "#{@name} runs to the #{destination}."
+  end
+
+  def report_age
+    puts "#{@name} is a #{@age} old"
   end
 end
 
 # And third class for Cats
 class Cat
+  attr_accessor :name
+
   def talk
-    puts 'Meow!'
+    puts "#{@name} says Meow!"
   end
 
   def move(destination)
-    puts "Running to the #{destination}."
+    puts "#{@name} runs to the #{destination}."
   end
 end
 
@@ -37,7 +47,12 @@ bird = Bird.new
 dog = Dog.new
 cat = Cat.new
 
-bird.move('tree')
+# bird.move('tree')
+dog.age = 5
+dog.name = 'Bary'
+dog.report_age
 dog.talk
+dog.move(500)
+bird.name = 'Chick'
 bird.talk
 cat.move('house')
